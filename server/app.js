a42e98ceef3b8b239dfe6ca1ec5bd4e647991616
Vaibhav-1901 <vaibhavsingh192007@gmail.com>
2025-09-05 15:18:54 +0530

    Access given to frontend

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ import express from "express";
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
-const app=express();
+const app = express();
 app.use(cors({
-    origin: "http://localhost:5173", 
-    credentials: true   
+  origin: [
+    "http://localhost:5173",                      // local dev
+    "https://blogging-platform-swart.vercel.app"  // deployed frontend
+  ],
+  credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json());
@@ -14,9 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 import userRouter from "./src/routes/user.router.js";
 import blogRouter from "./src/routes/blog.router.js";
 
-app.use("/api/users",userRouter)
-app.use("/api/blogs",blogRouter)
+app.use("/api/users", userRouter)
+app.use("/api/blogs", blogRouter)
 
-export {app}
+export { app }
 
 
